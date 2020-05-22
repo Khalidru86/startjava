54bbeb17127a841365dcd92cb4153f0ee4357082
Khalid <khalidru86@gmail.com>
2020-05-22 19:25:04 +0500

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+	
+public static void main(String[] args) {
+
+	byte kolichestvoYader = 2;
+	short pamyat = 4000;
+	int processor = 640000;
+	long versiya = 1909000000;
+	float vypusk = 10;
+	double sborka = 18363.778;
+	char intel = 87;
+	boolean windows = true;
+
+	System.out.println(kolichestvoYader);
+	System.out.println(pamyat);
+	System.out.println(processor);
+	System.out.println(versiya);
+	System.out.println(vypusk);
+	System.out.println(sborka);
+	System.out.println(intel);
+	System.out.println(windows);
+
+    }
+}
