3bc50387a8ffd0feb8af5c3820a8b766b348c938
Khalid <khalidru86@gmail.com>
2020-05-25 22:59:30 +0500

    changing variable names

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,12 +1,12 @@
 public class Variable {
 	
 public static void main(String[] args) {
-    byte kolichestvoYader = 2;
-	short pamyat = 4000;
+    byte numberOfCores = 2;
+	short memory = 4000;
 	int processor = 640000;
-	long versiya = 1909000000;
-	float vypusk = 10;
-	double sborka = 18363.778;
+	long version = 1909000000;
+	float release = 10;
+	double assembly = 18363.778;
 	char intel = 87;
 	boolean windows = true;
 
@@ -18,6 +18,5 @@ public static void main(String[] args) {
 	System.out.println(sborka);
 	System.out.println(intel);
 	System.out.println(windows);
-
     }
 }
