88fda352d011ad4a71413106ab14a645f0dcee3c
Khalid <khalidru86@gmail.com>
2020-05-26 16:04:06 +0500

    ConditionalStatement

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,20 +1,20 @@
 public class ConditionalStatement{
-	public static void main(String[] args) {
-	int myAge = 36;
-	if(myAge > 20) {
+    public static void main(String[] args) {
+    int myAge = 36;
+    if(myAge > 20) {
          System.out.println("Я могу быть дедушкой");
-	} 
-	boolean iAmMale = true;
-	if (iAmMale = true) {
-		System.out.println("Я могу быть папой");
-	}
+    } 
+    boolean iAmMale = true;
+    if (iAmMale = true) {
+        System.out.println("Я могу быть папой");
+    }
     boolean iAmNotMale = !false;
     if (iAmNotMale = !false) {
-    	System.out.println("Я не могу быть мамой");
+        System.out.println("Я не могу быть мамой");
     }
     double myHeight = 1.72;
     if (myHeight > 1.80) {
-    	System.out.println("Я высокий");
+        System.out.println("Я высокий");
     }
     else {System.out.println("Я среднего роста");
     }
