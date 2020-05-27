1e5ed3f7edc29f5fd2fb77bbc1f4538d64aba450
Khalid <khalidru86@gmail.com>
2020-05-27 11:48:37 +0500

    ConditionalStatement

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,34 +1,34 @@
-public class ConditionalStatement{
+public class ConditionalStatement {
     public static void main(String[] args) {
-    int myAge = 36;
-    if(myAge > 20) {
-         System.out.println("Я могу быть дедушкой");
-    } 
-    boolean iAmMale = true;
-    if (iAmMale = true) {
-        System.out.println("Я могу быть папой");
-    }
-    boolean iAmNotMale = !false;
-    if (iAmNotMale = !false) {
-        System.out.println("Я не могу быть мамой");
-    }
-    double myHeight = 1.72;
-    if (myHeight > 1.80) {
-        System.out.println("Я высокий");
-    }
-    else {System.out.println("Я среднего роста");
-    }
-    char myNameBeginsWithALetterD = 'D';
-    char myNameBeginsWithALetterM = 'D';
-    char myNameBeginsWithALetterI = 'D';
-    if (myNameBeginsWithALetterM == 'M') {
-        System.out.println("Меня зовут Миша");
-    }
-    else if (myNameBeginsWithALetterI == 'I') {
-        System.out.println("Меня зовут Илья");
-    }
-    else {
-        System.out.println("Меня зовут Даниил");
-    }
-  }
+      int isYourAge = 50;
+      if(isYourAge > 20) {
+         System.out.println("Ты можешь быть дедушкой");
+      } 
+
+      boolean isMale = true;
+      if(isMale = true) {
+          System.out.println("Ты можешь быть папой");
+      }
+
+      boolean isNotMale = !false;
+      if(isNotMale = !false) {
+          System.out.println("Ты не можешь быть мамой");
+      }
+
+      double isHeight = 1.72;
+      if(isHeight > 1.80) {
+          System.out.println("Ты высокий");
+      } else{
+          System.out.println("Ты среднего роста");
+      }
+
+      char isNameBeginsWithALetterD = 'D';
+      if(isNameBeginsWithALetterD == 'M') {
+          System.out.println("Тебя зовут Миша");
+      } else if(isNameBeginsWithALetterD == 'I') {
+          System.out.println("Тебя зовут Илья");
+      } else {
+        System.out.println("Тебя зовут Даниил");
+      }
+   }
 }
