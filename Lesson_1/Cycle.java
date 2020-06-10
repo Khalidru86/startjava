a8f298934631e6aa69254ee7ccdba14e5226f606
Khalid <khalidru86@gmail.com>
2020-06-10 16:17:53 +0500

    Cycle

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,18 @@
+public class Cycle {
+	public static void main (String[] args) {
+	   for(int i = 0; i < 21; i++) {
+	      System.out.print(i + " ");
+	   }
+
+	   int i = 6;
+	   while (i >= -6) {
+	      System.out.println(i + " ");
+	      i-=2;
+	   }
+
+	   // int i = 10;
+	   // int result = 1;
+	   // do {
+	   // 	   result = result + i;
+	}
+}	
