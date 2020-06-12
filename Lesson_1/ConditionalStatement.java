705fcd45f3d3ea83b5f33f960fd8994de5b41f22
Khalid <khalidru86@gmail.com>
2020-06-12 16:38:05 +0500

    ConditionalStatement

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,32 +1,32 @@
 public class ConditionalStatement {
-    public static void main(String[] args) {
-      int age = 50;
-      if(age > 20) {
-         System.out.println("Ты можешь быть дедушкой");
-      } 
+  public static void main(String[] args) {
+    int age = 50;
+    if(age > 20) {
+        System.out.println("Ты можешь быть дедушкой");
+    } 
 
-      boolean isMale = true;
-      if(isMale) {
-          System.out.println("Ты можешь быть папой");
-      }
-      if(!isMale) {
-          System.out.println("Ты не можешь быть папой");
-      }
+    boolean isMale = true;
+    if(isMale) {
+        System.out.println("Ты можешь быть папой");
+    }
+    if(!isMale) {
+        System.out.println("Ты не можешь быть папой");
+    }
 
-      double Height = 1.72;
-      if(Height > 1.80) {
-          System.out.println("Ты высокий");
-      } else {
-          System.out.println("Ты среднего роста");
-      }
+    double height = 1.72;
+    if(height > 1.80) {
+        System.out.println("Ты высокий");
+    } else {
+        System.out.println("Ты среднего роста");
+    }
 
-      char firstNameLetter = 'D';
-      if(firstNameLetter == 'M') {
-          System.out.println("Тебя зовут Миша");
-      } else if(firstNameLetter == 'I') {
-          System.out.println("Тебя зовут Илья");
-      } else {
+    char firstNameLetter = 'D';
+    if(firstNameLetter == 'M') {
+        System.out.println("Тебя зовут Миша");
+    } else if(firstNameLetter == 'I') {
+        System.out.println("Тебя зовут Илья");
+    } else {
         System.out.println("Тебя зовут Даниил");
-      }
-   }
+    }
+  }
 }
