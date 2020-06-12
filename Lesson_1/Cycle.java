78f5bceb442b97094c1e8872ec08e3a304b8dc25
Khalid <khalidru86@gmail.com>
2020-06-12 16:55:21 +0500

    Cycle

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,24 +1,24 @@
 public class Cycle {
-	public static void main (String[] args) {
-	   for(int i = 0; i < 21; i++) {
-	      System.out.print(i + " ");
-	   }
-
-	   int i = 6;
-	   while (i >= -6) {
-	      System.out.println(i + " ");
-	      i-= 2;
-	   }
-
-	   // int i = 9;
-	   // int result = 1;
-	   // do {
-	   //    result = result + i;
-	   //    i+=2;
-	   //    System.out.println(i + " ");
-	   // } while(i <= 18);
-	   // System.out.println("11 + 13 + 15 + 17 + 19 = " + result);
+  public static void main (String[] args) {
+    for(int i = 0; i < 21; i++) {
+      System.out.print(i + " ");
     }
+
+    int i = 6;
+    while (i >= -6) {
+      System.out.println(i + " ");
+      i-= 2;
+    }
+
+    // int i = 9;
+    // int result = 1;
+    // do {
+    //    result = result + i;
+    //    i+=2;
+    //    System.out.println(i + " ");
+    // } while(i <= 18);
+    // System.out.println("11 + 13 + 15 + 17 + 19 = " + result);
+  }
 }
 
 
