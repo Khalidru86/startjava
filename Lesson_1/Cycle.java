00b1fc20493d8c7b4493cd5c0553e0191d72c51b
Khalid <khalidru86@gmail.com>
2020-06-16 02:12:34 +0500

    change settings

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,24 +1,27 @@
 public class Cycle {
-  public static void main(String[] args) {
-    // for(int i = 0; i < 21; i++) {
-    //   System.out.println(i + " ");
-    // }
+	public static void main(String[] args) {
+		for(int i = 0; i < 21; i++) {
+			System.out.println(i + " ");
+		}
 
-    // int i = 6;
-    // while (i >= -6) {
-    //   System.out.print(i + " ");
-    //   i -= 2;
-    // }
+		int i = 6;
+		while (i >= -6) {
+			System.out.print(i + " ");
+		i -= 2;
+		}
 
-    int i = 9;
-    int result = 1;
-    do {
-       result = result + i;
-       i+=2;
-       System.out.println(i + " ");
-    } while(i < 19);
-    System.out.println("11 + 13 + 15 + 17 + 19 = " + result);
-  }
+		int i = 13;
+		int result = 11;
+		do {
+			result = result + i;
+			if(result % 2 == 0) {
+					System.out.println("Число " + result + " является четным");
+			} else {
+					System.out.println("Число " + result + " является нечетным");
+			}
+			i+=2;
+		} while(i <= 19);
+
+		System.out.println("11 + 13 +  15 + 17 + 19 = " + result);
+	}
 }
-
-
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-  public static void main(String[] args) {
-	byte numberOfCores = 2;
-	short memory = 4000;
-	int processor = 640000;
-	long version = 1909000000;
-	float release = 10;
-	double assembly = 18363.778;
-	char intel = 87;
-	boolean windows = true;
+	public static void main(String[] args) {
+		byte numberOfCores = 2;
+		short memory = 4000;
+		int processor = 640000;
+		long version = 1909000000;
+		float release = 10;
+		double assembly = 18363.778;
+		char intel = 87;
+		boolean windows = true;
 
-	System.out.println(numberOfCores);
-	System.out.println(memory);
-	System.out.println(processor);
-	System.out.println(version);
-	System.out.println(release);
-	System.out.println(assembly);
-	System.out.println(intel);
-	System.out.println(windows);
-  }
+		System.out.println(numberOfCores);
+		System.out.println(memory);
+		System.out.println(processor);
+		System.out.println(version);
+		System.out.println(release);
+		System.out.println(assembly);
+		System.out.println(intel);
+		System.out.println(windows);
+	}
 }
